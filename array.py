4f73c9a0911797c6f998b345a707eb17ec6c029b
Rishal-Jr <rmrazni@gmail.com>
2025-03-01 14:11:12 +0530

    array

---
+++ array.py
@@ -0,0 +1,8 @@
+import array as arr
+array_num=array.arr("i",[1,3,5,7,9,3,3])
+print("The original array"+str(array_num))
+
+print("Number of occurance of 3"+str(array_num.count(3)))
+
+array_num.reverse()
+print("The reversed array:"+str(array_num))
