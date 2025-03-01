96cda2833b3027423e01740c763cf9a7220259fb
Rishal-Jr <rmrazni@gmail.com>
2025-03-01 13:48:08 +0530

    sets

---
+++ a1.py
@@ -0,0 +1,17 @@
+myset={1,2,3}
+print(myset)
+
+myset={2.45,"Rishal",(1,2,3)}
+print(myset)
+
+myset={1,2,3,2,1}
+print(myset)
+
+myset=set([1,2,3,4,5,6])
+print(myset,"\n")
+
+num_set=set([0,1,2,3,4,5])
+print("Original set",num_set)
+
+num_set.pop()
+print("After removing the first element in the set",num_set,"\n")
